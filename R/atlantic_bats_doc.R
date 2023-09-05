8d9463c46fffa705c2fafd3371e1452a36dd27c8
mauriciovancine <mauricio.vancine@gmail.com>
2023-09-05 01:35:48 -0300

    update atlantic bats

---
+++ R/atlantic_bats_doc.R
@@ -0,0 +1,11 @@
+#' Atlantic Bats
+#'
+#' ATLANTIC BATS: a data set of bat communities from the Atlantic Forests of South America
+#'
+#' @format A tibble with 21,424 rows and 37 variables:
+#' \describe{
+#'   \item{id}{a number indicating the observation}
+#'   }
+#' @source {Muylaert et al. 2017. ATLANTIC BATS: a data set of bat communities from the Atlantic Forests of South America. Ecology.}
+#' \doi{10.1002/ecy.2007}
+"atlantic_bats"
---
+++ data-raw/atlantic_bats.R
@@ -0,0 +1,39 @@
+#' ---
+#' title: "atlantic amphibian data preparation"
+#' ---
+
+# prepare r -------------------------------------------------------------
+
+# packages
+library(tidyverse)
+
+# atlantic amphibians ----------------------------------------------------
+
+# download
+download.file(url = "https://raw.githubusercontent.com/LEEClab/Atlantic_series/master/ATLANTIC_BATS/DATASET/2018_02_d21/ATLANTIC_BATS_2020_comp.xlsx",
+              destfile = "data-raw/ATLANTIC_BATS_2020_comp.xlsx", mode = "wb")
+
+# import data
+atlantic_bats <- readxl::read_excel("data-raw/ATLANTIC_BATS_2020_comp.xlsx") %>%
+    janitor::clean_names() %>%
+    dplyr::select(-id_20, -id_137) %>%
+    dplyr::rename(id = id_1)
+
+# separate data
+atlantic_bats_sites <- atlantic_bats %>%
+    dplyr::select(c(1:31, 136:139))
+
+atlantic_bats_species <- atlantic_bats %>%
+    dplyr::select(c(1, 32:135)) %>%
+    tidyr::pivot_longer(cols = -1,
+                        names_to = "species",
+                        values_to = "abundance")
+
+# join
+atlantic_bats <- dplyr::left_join(atlantic_bats_sites, atlantic_bats_species)
+
+# save data
+usethis::use_data(atlantic_bats, overwrite = TRUE)
+
+# delete files
+unlink("data-raw/ATLANTIC_BATS_2020_comp.xlsx")
