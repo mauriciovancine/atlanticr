c7058cfa4a9189c5e8e593ecc77f0868d12f0aa9
mauriciovancine <mauricio.vancine@gmail.com>
2023-10-02 13:58:32 -0300

    update atlantic_spatial_download

--- R/atlantic_spatial_download.R
+++ R/atlantic_spatial_download.R
@@ -6,7 +6,6 @@
 #' @param scale  \cr
 #' @param edge_depth \cr
 #' @param gap_crossing \cr
-#' @param import \cr
 #' @param path \cr
 #'
 #' @example examples/atlantic_spatial_download_example.R
@@ -27,9 +26,8 @@ atlantic_spatial_download <- function(metric,
 
     # download
     googledrive::drive_deauth()
-    googledrive::drive_user()
     file_id <- googledrive::as_id("1yPGzwXYy8bYqV5xl31W8iFCXJUdRufsF")
     file <- googledrive::drive_get(file_id)
-    googledrive::drive_download(file)
+    googledrive::drive_download(file, overwrite = TRUE)
 
 }
