e63f020eed67d8477fa404edbdce5ea607dc3bf3
mauriciovancine <mauricio.vancine@gmail.com>
2023-11-07 10:21:41 -0300

    update atlantic spatial download

--- data-raw/atlantic_spatial.R
+++ data-raw/atlantic_spatial.R
@@ -11,7 +11,7 @@ library(janitor)
 # atlantic spatial ----------------------------------------------------
 
 # fix data
-atlantic_spatial <- readr::read_csv("data/atlantic_spatial_description.csv") %>%
+atlantic_spatial <- readr::read_csv("data-raw/atlantic_spatial_description.csv") %>%
     janitor::clean_names()
 
 # save data
